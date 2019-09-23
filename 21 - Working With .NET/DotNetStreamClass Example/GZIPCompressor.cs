410ecb9a664dd0069e4fef9fafe36710311e0f74
Simon Key <sdckey@users.noreply.github.com>
2019-09-23 14:00:44 +0100

    Added DotNetStreamClass  example

---
+++ 21 - Working With .NET/DotNetStreamClass Example/GZIPCompressor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace StreamTester
+{
+    public static class GZIPCompressor
+    {
+        public static void Compress(String path, Stream memStream)
+        {
+            using (memStream)
+            {
+                using (FileStream compressedFileStream = File.Create(path))
+                {
+                    using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                    {
+                        memStream.CopyTo(compressionStream);
+                    }
+                }
+            }
+        }
+    }
+}
