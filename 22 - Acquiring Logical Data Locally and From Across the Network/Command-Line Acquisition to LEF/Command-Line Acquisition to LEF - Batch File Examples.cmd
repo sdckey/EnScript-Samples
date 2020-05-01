6ac47c172de71ffd8a8633441809cf0e1f09cfb2
Simon Key <sdckey@users.noreply.github.com>
2020-05-01 17:25:06 +0100

    Added command-line acquisition to LEF example

---
+++ 22 - Acquiring Logical Data Locally and From Across the Network/Command-Line Acquisition to LEF/Command-Line Acquisition to LEF - Batch File Examples.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+"c:\Program Files\EnCase 20.2.0.185\EnCase.exe" -x -r -min "c:\Users\simon.key\Desktop\Command-Line Acquisition to LEF.EnScript" "\\SLOHVHOST_2\Student" "jpg,jpeg,png" "C:\JPEG and PNG Files From Network Share.Lx01"
+"c:\Program Files\EnCase 20.2.0.185\EnCase.exe" -x -r -min "c:\Users\simon.key\Desktop\Command-Line Acquisition to LEF.EnScript" "C:" "docx,xlsx,pptx" "C:\MS Office Files From the C Volume.Lx01"
