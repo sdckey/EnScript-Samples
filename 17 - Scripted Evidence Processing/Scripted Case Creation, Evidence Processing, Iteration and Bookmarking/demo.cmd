f192b1a6dcc1334a86d54598cad2c9d915df50fe
Simon Key <sdckey@users.noreply.github.com>
2026-07-07 17:29:38 +0100

    Bug fixes
    Fixed one bug whereby the wrong evidence cache folder was being used.
    
    Also fixed an issue detecting when evidence processing has completed.

--- 17 - Scripted Evidence Processing/Scripted Case Creation, Evidence Processing, Iteration and Bookmarking/demo.cmd
+++ 17 - Scripted Evidence Processing/Scripted Case Creation, Evidence Processing, Iteration and Bookmarking/demo.cmd
@@ -1,12 +1,12 @@
 @echo off
 
-set encase_path="C:\Program Files\EnCase V21.1\EnCase.exe"
+set encase_path="C:\Program Files\OpenText\OpenText Forensic 26.2\EnCase.exe"
 
 set case_path="C:\Demo\Demo Case\Demo Case.case"
 
 set script_path="C:\Demo\Scripted Case Creation, Evidence Processing, Iteration and Bookmarking.EnScript"
 
-set evidence_path="C:\Users\simon.key\Desktop\Evidence Sets\HOWE WIN10"
+set evidence_path="C:\Demo\Demo Evidence"
 
 echo.
 echo Starting EnCase. Maximize the application's window to view the Console output. 
